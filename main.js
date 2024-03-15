992aa0ab97a1319703722e1e56e733ce892fd190
Josu06 <163418499+Josu06@users.noreply.github.com>
2024-03-15 10:06:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+ document.getElementById("imagem").src="https://cdn.britannica.com/73/234573-050-8EE03E16/Cristiano-Ronaldo-ceremony-rename-airport-Santa-Cruz-Madeira-Portugal-March-29-2017.jpg"
+}
+   
+function mudarimagemdentro()
+  { 
+    document.getElementById("imagem").src="https://s.france24.com/media/display/fbe1d97e-6a8e-11ed-8243-005056bfb2b6/w:980/p:16x9/000_32KV2M6.jpg"
+ }
